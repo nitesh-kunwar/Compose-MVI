74af715b88db1f28ffe619ab0bc464d8154ec870
Nitesh <nitesh.singh@appinventiv.com>
2021-06-28 18:05:23 +0530

    added imdb link on readme

--- app/src/main/java/com/demo/compose/data/network/ApiService.kt
+++ app/src/main/java/com/demo/compose/data/network/ApiService.kt
@@ -11,6 +11,7 @@ import retrofit2.http.Query
 interface ApiService {
 
 
+
     @Headers("x-rapidapi-key: de2f155dc4msh92ff4135a3d3c4fp1d3fefjsncedf95ba5786")
     @GET("/auto-complete")
     fun searchMovie(@Query("q") queryString: String): Call<MovieModel>
